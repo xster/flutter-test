30e338f204d00e7ecfb5ce3bb048364f3f7ed54e
Xiao Yu <xster@google.com>
2017-08-07 12:26:22 -0700

    After creating the splash engine

---
+++ t35_splash_created_with_template/README.md
@@ -0,0 +1,8 @@
+# t35_created_with_template
+
+A new Flutter project.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](http://flutter.io/).
---
+++ t35_splash_created_with_template/android/app/src/main/java/com/yourcompany/t35createdwithtemplate/MainActivity.java
@@ -0,0 +1,16 @@
+package com.yourcompany.t35createdwithtemplate;
+
+import android.os.Bundle;
+
+import com.yourcompany.t35createdwithtemplate.R;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}

5c48330514d3ad7864415173bcc28d939b9e04d3
Xiao Yu <xster@google.com>
2017-09-15 13:13:49 -0700

    t46

--- t45_mail_list_text_question/lib/main.dart
+++ t45_mail_list_text_question/lib/main.dart
@@ -11,101 +11,193 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new bottomnavbar(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class bottomnavbar extends StatefulWidget {
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  State<StatefulWidget> createState() {
+    return new _bottomnavbar();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _bottomnavbar extends State<bottomnavbar> {
+  /// This controller can be used to programmatically
+  /// set the current displayed page
+  // PageController _pageController;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  // int _page = 0;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '${_counter}',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        // appBar: new AppBar(
+        //   // Here we take the value from the MyHomePage object that was created by
+        //   // the App.build method, and use it to set our appbar title.
+        //   title: new Text("Settings"), automaticallyImplyLeading: false,
+        //   actions: <Widget>[
+        //     new IconButton(
+        //       // action button
+        //       icon: new Icon(Icons.check),
+        //       onPressed: () {
+        //         null;
+        //       },
+        //     ),
+        //   ],
+        //   backgroundColor: Colors.indigo,
+        // ),
+        body: new PageView(
+            children: [
+              new settings(),
+              // new settings(),
+              // new settings(),
+              // new settings()
+            ],
+
+            /// Specify the page controller
+            // controller: _pageController,
+            // onPageChanged: onPageChanged
         ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+            // bottomNavigationBar: new BottomNavigationBar(
+            //   items: [
+            //     new BottomNavigationBarItem(
+            //         icon: new Icon(Icons.account_circle), title: new Text("z")),
+            //     new BottomNavigationBarItem(
+            //         icon: new Icon(Icons.people), title: new Text("a")),
+            //     new BottomNavigationBarItem(
+            //         icon: new Icon(Icons.tune), title: new Text("b")),
+            //     new BottomNavigationBarItem(
+            //         icon: new Icon(Icons.person), title: new Text("c"))
+            //   ],
+            // /// Will be used to scroll to the next page
+            // /// using the _pageController
+            // onTap: navigationTapped,
+            // currentIndex: _page)
+        );
+  }
+
+  // /// Called when the user presses on of the
+  // /// [BottomNavigationBarItem] with corresponding
+  // /// page index
+  // void navigationTapped(int page) {
+  //   // Animating to the page.
+  //   // You can use whatever duration and curve you like
+  //   _pageController.animateToPage(page,
+  //       duration: const Duration(milliseconds: 10), curve: Curves.ease);
+  // }
+
+  // void onPageChanged(int page) {
+  //   setState(() {
+  //     this._page = page;
+  //   });
+  // }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _pageController = new PageController();
+  // }
+
+  // @override
+  // void dispose() {
+  //   super.dispose();
+  //   _pageController.dispose();
+  // }
+}
+
+class settings extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+
+        body: new ListView.builder(
+          itemCount: 40,
+          itemExtent: 40.0,
+          itemBuilder: (BuildContext context, int index) {
+            return new TextField(
+              decoration: new InputDecoration(
+                hintText: index.toString(),
+              ),
+            );
+            // return new Row(
+            //   children: <Widget>[
+            //     // new Text(index.toString()),
+            //     new TextField(),
+            //   ],
+            // );
+          },
+        ),
+          // children: [
+
+            // new Container(
+            //   height: 50.0,
+            //   color: Colors.teal,
+            //   padding: const EdgeInsets.only(
+            //     top: 14.0,
+            //     left: 8.0,
+            //   ),
+            //   child: new Text('''DEFAULT SETTINGS''',
+            //       softWrap: true,
+            //   ),
+            // ),
+            // new Container(
+            //   padding: const EdgeInsets.only(
+            //       top: 0.0, left: 8.0, right: 8.0, bottom: 0.0),
+
+            //   child: new TextField(
+            //     decoration: new InputDecoration(
+            //       hintText: 'xyzzy',
+            //       labelText: 'xyzzy',
+            //     ),
+            //     textAlign: TextAlign.left,
+            //   ),
+            // ),
+            // new Container(
+            //   padding: const EdgeInsets.only(
+            //       top: 0.0, left: 8.0, right: 8.0, bottom: 0.0),
+
+            //   child: new TextField(
+            //     decoration: new InputDecoration(
+            //       hintText: 'xyzzy',
+            //       labelText: 'xyzzy',
+            //     ),
+            //     textAlign: TextAlign.left,
+            //   ),
+            // ),
+            // new Container(
+            //   height: 50.0,
+            //   color: Colors.tealAccent,
+            //   padding: const EdgeInsets.only(
+            //     top: 14.0,
+            //     left: 8.0,
+            //   ),
+            //   child: new Text('''xyzzy''',
+            //       softWrap: true,
+            //   ),
+            // ),
+            // new Container(
+            //   padding: const EdgeInsets.only(
+            //       top: 0.0, left: 8.0, right: 8.0, bottom: 0.0),
+            //   child: new TextField(
+            //     decoration: new InputDecoration(
+            //       hintText: 'xyzzy',
+            //       labelText: 'xyzzy',
+            //     ),
+            //     textAlign: TextAlign.left,
+            //   ),
+            // ),
+            // new Container(
+            //   child: new TextField(),
+            // ),
+            // new Container(
+            //   child: new TextField(),
+            // ),
+          // ],
+        // )
+      );
   }
 }
---
+++ t46_reduced_test_from_t45/README.md
@@ -0,0 +1,8 @@
+# t46_reduced_test_from_t45
+
+A new Flutter project.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](http://flutter.io/).

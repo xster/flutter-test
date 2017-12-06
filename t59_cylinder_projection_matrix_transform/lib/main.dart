7d71aa2d2bbb65cf99efb4076c35a68d2f313a8a
Xiao Yu <xster@google.com>
2017-12-05 18:16:35 -0800

    projection kinda works, going back to rotating model

--- t59_cylinder_projection_matrix_transform/lib/main.dart
+++ t59_cylinder_projection_matrix_transform/lib/main.dart
@@ -29,11 +29,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  PageController pageController;
+  bool alternateMode = false;
+
   double cameraHeight = 1.0;
   double modelScale = 1.0;
   double rotationAngle = 0.0;
 
+  double perspectiveProjection = 0.0;
+
   _MyHomePageState();
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = new PageController(initialPage: 0);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,74 +58,47 @@ class _MyHomePageState extends State<MyHomePage> {
             children: <Widget>[
               new Expanded(
                 child: new Center(
-                  child: new Transform(
-                    transform: _getNewTransform(),
-                    alignment: Alignment.center,
-                    child: new Container(
-                      width: 200.0,
-                      height: 150.0,
-                      decoration: const BoxDecoration(
-                        gradient: const LinearGradient(
-                          colors: const <Color>[
-                            Colors.blue, Colors.red,
-                          ],
-                        ),
-                      ),
-                      child: new Center(
-                        child: const Text(
-                          '5\nABC',
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(
-                            fontSize: 60.0,
-                            color: Colors.yellow,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
+                  child: buildTargetWidget(),
                 ),
               ),
-              new Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: new Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+              new Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const Text('Perspective View Model'),
+                  new Switch(
+                    value: alternateMode,
+                    onChanged: (bool value) {
+                      setState(() {
+                        alternateMode = value;
+                        pageController.jumpToPage(value ? 1 : 0);
+                      });
+                    },
+                  ),
+                  const Text('Perspective Projection'),
+                ],
+              ),
+              new LimitedBox(
+                maxHeight: 350.0,
+                child: new PageView(
+                  controller: pageController,
+                  scrollDirection: Axis.horizontal,
                   children: <Widget>[
-                    const Text('Camera Height (view z)'),
-                    new Slider(
-                      label: cameraHeight.toStringAsFixed(2),
-                      value: cameraHeight,
-                      min: 0.0,
-                      max: 200.0,
-                      onChanged: onCameraHeightChanged,
-                    ),
-                    const Text('Model scale'),
-                    new Slider(
-                      label: modelScale.toStringAsFixed(2),
-                      value: modelScale,
-                      min: 0.1,
-                      max: 20.0,
-                      onChanged: onModelScaleChanged,
-                    ),
-                    const Text('Camera Angle'),
-                    new Slider(
-                      label: rotationAngle.toStringAsFixed(2),
-                      value: rotationAngle,
-                      min: -pi / 2.0,
-                      max: pi / 2.0,
-                      onChanged: onRotationAngleChanged,
-                    ),
-                    new RaisedButton(
-                      child: const Text('Reset'),
-                      onPressed: () {
-                        setState(() {
-                          cameraHeight = 1.0;
-                          modelScale = 1.0;
-                          rotationAngle = 0.0;
-                        });
-                      },
-                    )
+                    buildPVMControlPanel(),
+                    buildPerspectiveMatrixControlPanel(),
                   ],
                 ),
+              ),
+              new RaisedButton(
+                child: const Text('Reset'),
+                onPressed: () {
+                  setState(() {
+                    cameraHeight = 1.0;
+                    modelScale = 1.0;
+                    rotationAngle = 0.0;
+
+                    perspectiveProjection = 0.0;
+                  });
+                },
               ),
             ],
           ),
@@ -123,32 +107,125 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  void onCameraHeightChanged(double value) {
-    setState(() {
-      cameraHeight = value;
-    });
-  }
-  void onModelScaleChanged(double value) {
-    setState(() {
-      modelScale = value;
-    });
+  Widget buildTargetWidget() {
+    return new Transform(
+      transform: alternateMode ? getPerspectiveProjectionTransform() : getPVMTransform(),
+      alignment: Alignment.center,
+      child: new Container(
+        width: 200.0,
+        height: 150.0,
+        decoration: const BoxDecoration(
+          gradient: const LinearGradient(
+            colors: const <Color>[
+              Colors.blue, Colors.red,
+            ],
+          ),
+        ),
+        child: new Center(
+          child: const Text(
+            '5\nABC',
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 60.0,
+              color: Colors.yellow,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
-  void onRotationAngleChanged(double value) {
-    setState(() {
-      rotationAngle= value;
-    });
+  Widget buildPVMControlPanel() {
+    return new Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const Text('Camera Height (view z)'),
+          new Slider(
+            label: cameraHeight.toStringAsFixed(2),
+            value: cameraHeight,
+            min: 0.0,
+            max: 200.0,
+            onChanged: (double value) { setState(() { cameraHeight = value; }); },
+          ),
+          const Text('Model scale'),
+          new Slider(
+            label: modelScale.toStringAsFixed(2),
+            value: modelScale,
+            min: 0.1,
+            max: 20.0,
+            onChanged: (double value) { setState(() { modelScale = value; }); },
+          ),
+          const Text('Camera Angle'),
+          new Slider(
+            label: rotationAngle.toStringAsFixed(2),
+            value: rotationAngle,
+            min: -pi / 2.0,
+            max: pi / 2.0,
+            onChanged: (double value) { setState(() { rotationAngle = value; }); },
+          ),
+          const Text('Perspective projection'),
+          new Slider(
+            label: perspectiveProjection.toStringAsFixed(2),
+            value: perspectiveProjection,
+            min: 0.0,
+            max: 0.01,
+            onChanged: (double value) { setState(() { perspectiveProjection = value; }); },
+          ),
+        ],
+      ),
+    );
   }
 
-  Matrix4 _getNewTransform() {
+  Widget buildPerspectiveMatrixControlPanel() {
+    return new Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const Text('Perspective projection'),
+          new Slider(
+            label: perspectiveProjection.toStringAsFixed(2),
+            value: perspectiveProjection,
+            min: 0.0,
+            max: 0.01,
+            onChanged: (double value) { setState(() { perspectiveProjection = value; }); },
+          ),
+          const Text('Rotation Angle'),
+          new Slider(
+            label: rotationAngle.toStringAsFixed(2),
+            value: rotationAngle,
+            min: -pi / 2.0,
+            max: pi / 2.0,
+            onChanged: (double value) { setState(() { rotationAngle = value; }); },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Matrix4 getPerspectiveProjectionTransform() {
+    final Matrix4 transformation = (new Matrix4.identity()
+        ..setEntry(3, 2, perspectiveProjection)) * new Matrix4.rotationX(rotationAngle);
+
+    print(transformation);
+    return transformation;
+  }
+
+  Matrix4 getPVMTransform() {
     var cameraPosition = new Math.Vector3(0.0, cameraHeight * sin(rotationAngle), cameraHeight * cos(rotationAngle));
     var cameraFocusPosition = new Math.Vector3(0.0, 0.0, 0.0);
     var cameraUp = new Math.Vector3(0.0, 1.0, 0.0);
 
     var projectionMatrix = Math.makePerspectiveMatrix(90.0 * (PI / 180.0), 1.0, 1.0, 1000.0);
+    print(projectionMatrix);
+    projectionMatrix = new Matrix4.identity()
+        ..setEntry(3, 2, -perspectiveProjection);
+    print(projectionMatrix);
     var viewMatrix = Math.makeViewMatrix(cameraPosition, cameraFocusPosition, cameraUp);
 
-    var modelMatrix = new Matrix4.diagonal3Values(modelScale, modelScale, 1.0);
+    var modelMatrix = new Matrix4.diagonal3Values(modelScale, 1.0, 1.0);
     return projectionMatrix * viewMatrix * modelMatrix;
   }
 }

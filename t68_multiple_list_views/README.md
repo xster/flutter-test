4bc1a6d25ddcf27e12e7c4d83249c603c9082a5c
Xiao Yu <xster@google.com>
2018-01-17 14:41:12 -0800

    t67, t68

---
+++ t68_multiple_list_views/README.md
@@ -0,0 +1,8 @@
+# t68_multiple_list_views
+
+A new Flutter project.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](http://flutter.io/).
---
+++ t68_multiple_list_views/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+        // counter didn't reset back to zero; the application is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: new Scaffold(
+        body: new Row(
+          children: <Widget>[
+            new SizedBox(
+              width: 100.0,
+              child: new ListView(
+                children: new List<Widget>.generate(100, (int index) {
+                  return new Container(
+                    height: 100.0,
+                    color: const Color(0xFF0000FF),
+                    margin: const EdgeInsets.all(10.0),
+                  );
+                }),
+              ),
+            ),
+            new SizedBox(
+              width: 100.0,
+              child: new ListView(
+                children: new List<Widget>.generate(100, (int index) {
+                  return new Container(
+                    height: 100.0,
+                    color: const Color(0xFF0000FF),
+                    margin: const EdgeInsets.all(10.0),
+                  );
+                }),
+              ),
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}

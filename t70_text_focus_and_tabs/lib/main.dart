66cc5ae00900023e78140fdba70bebb57eb55d83
Xiao Yu <xster@google.com>
2018-02-14 17:41:36 -0800

    Some clean up

--- t70_text_focus_and_tabs/lib/main.dart
+++ t70_text_focus_and_tabs/lib/main.dart
@@ -9,36 +9,55 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      home: new CupertinoTabScaffold(
-        tabBar: new CupertinoTabBar(
-          items: <BottomNavigationBarItem>[
-            new BottomNavigationBarItem(
-              icon: new Icon(Icons.directions_car),
-              title: new Text('Left'),
-            ),
-            new BottomNavigationBarItem(
-              icon: new Icon(Icons.directions_railway),
-              title: new Text('Right'),
-            ),
-          ],
-        ),
-        tabBuilder: (BuildContext context, int index) {
-          return new CupertinoTabView(
-            builder: (BuildContext context) {
-              return new CupertinoPageScaffold(
-                navigationBar: new CupertinoNavigationBar(
-                  middle: new Text(index == 0 ? 'Left' : 'Right'),
-                ),
-                child: new Material(
-                  child: new Container(
-                    padding: const EdgeInsets.only(top: 100.0),
-                    child: new TextField(),
+      home: new SafeArea(
+        bottom: false,
+        child: new Material(
+          color: Colors.yellow,
+          child: new Column(
+            children: <Widget>[
+              new TextField(
+                decoration: const InputDecoration(hintText: 'Text outside tabs'),
+              ),
+              new SizedBox(
+                height: 600.0,
+                child: new CupertinoTabScaffold(
+                  tabBar: new CupertinoTabBar(
+                    items: <BottomNavigationBarItem>[
+                      new BottomNavigationBarItem(
+                        icon: new Icon(Icons.directions_car),
+                        title: new Text('Left'),
+                      ),
+                      new BottomNavigationBarItem(
+                        icon: new Icon(Icons.directions_railway),
+                        title: new Text('Right'),
+                      ),
+                    ],
                   ),
+                  tabBuilder: (BuildContext context, int index) {
+                    return new CupertinoTabView(
+                      builder: (BuildContext context) {
+                        return new CupertinoPageScaffold(
+                          navigationBar: new CupertinoNavigationBar(
+                            middle: new Text(index == 0 ? 'Left' : 'Right'),
+                          ),
+                          child: new Material(
+                            child: new Container(
+                              padding: const EdgeInsets.only(top: 100.0),
+                              child: new Column(children: <Widget>[
+                                new TextField(),
+                                new TextField(),
+                              ]),
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
                 ),
-              );
-            },
-          );
-        },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }

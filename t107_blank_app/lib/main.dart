5dae3fab89bc74bbe4159a8d3b9dbd4fe8d22c14
Xiao Yu <xiao@xster.net>
2018-08-25 22:45:00 -0700

    107

---
+++ t107_blank_app/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+  }
+}

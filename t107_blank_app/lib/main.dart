01596871c7f1d55b848abae5d9982be420c4457f
Xiao Yu <xiao@xster.net>
2018-08-28 15:45:49 -0700

    test diagram with empty project

--- t107_blank_app/lib/main.dart
+++ t107_blank_app/lib/main.dart
@@ -1,10 +1,4 @@
+import 'package:diagrams/diagrams.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-  }
-}
+void main() => runApp(HeroesDiagram());

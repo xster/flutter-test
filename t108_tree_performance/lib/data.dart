55e7394e042e9d47c22f84279f88f29952b6e3bf
Xiao Yu <xster@google.com>
2018-09-18 10:52:02 -0700

    more cleanups

--- t108_tree_performance/lib/data.dart
+++ t108_tree_performance/lib/data.dart
@@ -4,8 +4,3 @@ import 'dart:ui';
 
 Stream<int> promotions =
     new Stream.periodic(const Duration(seconds: 1), (int count) => count).asBroadcastStream();
-
-Random random = new Random();
-
-Color getRandomColor() => new Color.fromARGB(0xFF, 100 + random.nextInt(155),
-    100 + random.nextInt(155), 100 + random.nextInt(155));

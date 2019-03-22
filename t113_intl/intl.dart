11170045de1923697efea0bd6cad821abcbb1494
Xiao Yu <xster@google.com>
2019-03-22 14:40:04 -0700

    more intl tests

--- t113_intl/intl.dart
+++ t113_intl/intl.dart
@@ -1,14 +1,32 @@
-import 'dart:async';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
-  Intl.defaultLocale = 'zh';
-  await initializeDateFormatting('zh', null);
-  print(DateFormat.y().format(DateTime.now()));
-  print(DateFormat.yMd().format(DateTime.now()));
+  Intl.defaultLocale = 'en';
+  await initializeDateFormatting('en', null);
+  print('English');
+  testStuff();
+  Intl.defaultLocale = 'zh_CN';
+  await initializeDateFormatting('zh_CN', null);
+  print('Chinese');
+  testStuff();
   Intl.defaultLocale = 'ar';
   await initializeDateFormatting('ar', null);
-  print(DateFormat.m().format(DateTime.now()));
-  print(DateFormat.yMd().format(DateTime.now()));
+  print('Arabic');
+  testStuff();
+  Intl.defaultLocale = 'ko';
+  await initializeDateFormatting('ko', null);
+  print('Korean');
+  testStuff();
+}
+
+void testStuff() {
+  print(DateFormat.y().format(DateTime.utc(1995)));
+  print(DateFormat.MMMM().format(DateTime.now()));
+  print(DateFormat.MMMEd().format(DateTime.now()));
+  print(DateFormat.d().format(DateTime.utc(0, 0, 4)));
+  print(DateFormat.H().format(DateTime.utc(0, 0, 0, 13)));
+  print(DateFormat('HH').format(DateTime.utc(0, 0, 0, 4)));
+  print(DateFormat().add_H().format(DateTime.utc(0, 0, 0, 4)));
+  print('\n\n');
 }

c3fd9eae7c2a92f221314c45321c4c48dbe0c121
Xiao Yu <xster@google.com>
2019-03-25 13:30:35 -0700

    t120 init

---
+++ t120_picker_refresh/android/app/src/main/java/com/example/t120_picker_refresh/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.t120_picker_refresh;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}

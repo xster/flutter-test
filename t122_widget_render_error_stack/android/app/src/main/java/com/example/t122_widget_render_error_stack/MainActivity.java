daceb9ad852be37d2f62d24dcf0ecb26409ab5f9
Xiao Yu <xster@google.com>
2019-03-26 17:44:11 -0700

    t122 init

---
+++ t122_widget_render_error_stack/android/app/src/main/java/com/example/t122_widget_render_error_stack/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.t122_widget_render_error_stack;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}

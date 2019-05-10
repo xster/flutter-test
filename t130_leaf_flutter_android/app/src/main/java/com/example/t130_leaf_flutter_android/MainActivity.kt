d2e45d614fcbcdecdbd3a3f91a3aeb2b56f96f3c
Xiao Yu <xster@google.com>
2019-05-10 02:36:11 -0700

    there is a basic android layout, no flutter yet

--- t130_leaf_flutter_android/app/src/main/java/com/example/t130_leaf_flutter_android/MainActivity.kt
+++ t130_leaf_flutter_android/app/src/main/java/com/example/t130_leaf_flutter_android/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.t130_leaf_flutter_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        progressSlider.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                progressBar.progress = progress
+                progressLabel.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) { }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) { }
+        })
+    }
+
+    fun goToFlutter(view: View) {
+
     }
 }

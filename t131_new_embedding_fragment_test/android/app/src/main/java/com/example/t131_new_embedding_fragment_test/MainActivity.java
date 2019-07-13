bbac067d4d81e764981908ced372008c63d0f4ca
Xiao Yu <xster@google.com>
2019-07-12 17:03:06 -0700

    131

---
+++ t131_new_embedding_fragment_test/android/app/src/main/java/com/example/t131_new_embedding_fragment_test/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.t131_new_embedding_fragment_test;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}

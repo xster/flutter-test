e9104c11e3acb0257c12e48feabe4848065b050a
Xiao Yu <xster@google.com>
2019-07-29 11:23:16 -0700

    no facade test

--- t133_android_test_without_facade/app/src/main/java/com/example/t133_android_test_without_facade/MainActivity.kt
+++ t133_android_test_without_facade/app/src/main/java/com/example/t133_android_test_without_facade/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.t133_android_test_without_facade
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import io.flutter.app.FlutterActivity
+import io.flutter.view.FlutterMain
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        FlutterMain.startInitialization(applicationContext)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
---
+++ t133_android_test_without_facade_flutter/README.md
@@ -0,0 +1,8 @@
+# t133_android_test_without_facade_flutter
+
+A new flutter module project.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.dev/).

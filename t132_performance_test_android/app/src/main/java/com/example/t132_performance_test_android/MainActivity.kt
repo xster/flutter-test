05a20d568bb70be3c7b61b2e108adcaee2000455
Xiao Yu <xster@google.com>
2019-08-08 18:51:33 -0700

    mini progress

--- t132_performance_test_android/app/src/main/java/com/example/t132_performance_test_android/MainActivity.kt
+++ t132_performance_test_android/app/src/main/java/com/example/t132_performance_test_android/MainActivity.kt
@@ -5,8 +5,13 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun engineInit() {
+
     }
 }

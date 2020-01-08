ab7f23593ea33fc3069777f5c41df49ffcea88b1
Xiao Yu <xiao@xster.net>
2020-01-07 18:12:45 -0800

    149

---
+++ t149_new_platform_channel/android/app/src/main/java/com/example/t149_new_platform_channel/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.t149_new_platform_channel;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  private static final String CHANNEL = "samples.flutter.dev/battery";
+
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+    new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+        .setMethodCallHandler(
+          (call, result) -> {
+
+          }
+        );
+  }
+}

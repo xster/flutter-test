bbd639e84e40870eb0ff7b300cfcac7fce1c1132
Xiao Yu <xiao@xster.net>
2020-03-30 15:36:52 -0700

    working test for crashlytics

--- t154_android_crashlytics/app/src/main/java/com/example/t154_android_crashlytics/MainActivity.kt
+++ t154_android_crashlytics/app/src/main/java/com/example/t154_android_crashlytics/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.t154_android_crashlytics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.crashlytics.android.Crashlytics
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onCrash(view: View) {
+        Crashlytics.getInstance().crash()
+    }
+
+    fun runFlutter(view: View) {
+        startActivity(FlutterActivity.createDefaultIntent(this))
     }
 }

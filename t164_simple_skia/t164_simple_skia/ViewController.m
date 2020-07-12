6a542d1c0255a77dada87d7407773c311193ab4f
Xiao Yu <xiao@xster.net>
2020-07-12 16:04:50 -0700

    add t164

---
+++ t164_simple_skia/t164_simple_skia/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  t164_simple_skia
+//
+//  Created by Xiao Yu on 6/11/20.
+//  Copyright © 2020 Xiao Yu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end

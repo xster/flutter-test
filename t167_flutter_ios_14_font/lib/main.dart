1a9380425776d9a74f6c2fcfac8cfd6b39755496
Xiao Yu <xiao@xster.net>
2020-08-12 23:08:34 -0700

    test with multiple fonts

--- t167_flutter_ios_14_font/lib/main.dart
+++ t167_flutter_ios_14_font/lib/main.dart
@@ -9,14 +9,40 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return CupertinoApp(
       home: CupertinoPageScaffold(child: Center(
-        child: Text(
-          'Default system 17 font',
-          style: TextStyle(
-            fontFamily: '.SFUI-Regular',
-            fontSize: 17.0,
-            fontWeight: FontWeight.w400,
-            letterSpacing: 0.36
-          ),
+        child: Column(
+          children: [
+            Text(
+              'Arial 70',
+              style: TextStyle(
+                fontFamily: 'Arial',
+                fontSize: 70
+              )
+            ),
+            Text(
+              '| | | | | | | | | | Default system 17 font | | | | | | | | | |',
+              style: TextStyle(
+                fontFamily: '.SFUI-Regular',
+                fontSize: 17.0,
+                fontWeight: FontWeight.w400,
+                letterSpacing: -0.41,
+                // letterSpacing: 0.36
+              ),
+            ),
+            Text(
+              '| | | | | | | | | | System Arial 17 font | | | | | | | | | |',
+              style: TextStyle(
+                fontFamily: 'Arial',
+                fontSize: 17.0
+              )
+            ),
+            Text(
+              '| | | | | | | | | | Open Sans 17 font | | | | | | | | | |',
+              style: TextStyle(
+                fontFamily: 'Open Sans',
+                fontSize: 17.0
+              )
+            )
+          ],
         ),
       )),
     );

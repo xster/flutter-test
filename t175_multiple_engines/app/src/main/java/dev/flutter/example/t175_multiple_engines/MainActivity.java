6765c6308f3a3af933b6659184b173f6e02d180b
Xiao Yu <xiao@xster.net>
2020-10-21 13:57:43 -0700

    start putting in stuff

--- t175_multiple_engines/app/src/main/java/dev/flutter/example/t175_multiple_engines/MainActivity.java
+++ t175_multiple_engines/app/src/main/java/dev/flutter/example/t175_multiple_engines/MainActivity.java
@@ -3,12 +3,22 @@ package dev.flutter.example.t175_multiple_engines;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+  SeekBar engineQuantitySeekBar;
+  Button launchFlutterButton;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    engineQuantitySeekBar = findViewById(R.id.seekBar);
+    launchFlutterButton = findViewById(R.id.button);
+
+
   }
 }
---
+++ t175_multiple_engines/app/src/main/java/dev/flutter/example/t175_multiple_engines/MultipleFlutterActivity.java
@@ -0,0 +1,5 @@
+package dev.flutter.example.t175_multiple_engines;
+
+public class MultipleFlutterActivity {
+
+}

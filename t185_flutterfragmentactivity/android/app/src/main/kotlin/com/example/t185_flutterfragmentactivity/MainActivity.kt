0c4c2d7070956f49982aed9322c9f831d371a438
Xiao Yu <xiao@xster.net>
2021-04-29 22:25:12 -0700

    repro

--- t185_flutterfragmentactivity/android/app/src/main/kotlin/com/example/t185_flutterfragmentactivity/MainActivity.kt
+++ t185_flutterfragmentactivity/android/app/src/main/kotlin/com/example/t185_flutterfragmentactivity/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.t185_flutterfragmentactivity
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
